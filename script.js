e283051461ccc560c5de2092416596707cdaceaf
abdessamadmounirsaidoune-hash <abdessamadmounirsaidoune@gmail.com>
2026-03-08 00:16:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
 async function askAI() {
 
-const question = document.getElementById("question").value;
+let question = document.getElementById("question").value;
 
 document.getElementById("answer").innerText = "جاري التفكير...";
 
-const response = await fetch("https://cold-mountain-df10.abdessamadmounirsaidoune.workers.dev", {
+try {
+
+let response = await fetch("https://cold-mountain-df10.abdessamadmounirsaidoune.workers.dev", {
+
 method: "POST",
+
 headers: {
 "Content-Type": "application/json"
 },
+
 body: JSON.stringify({
 model: "openai/gpt-3.5-turbo",
 messages: [
-{ role: "user", content: question }
+{
+role: "user",
+content: question
+}
 ]
 })
+
 });
 
-const data = await response.json();
+let data = await response.json();
 
 document.getElementById("answer").innerText =
 data.choices[0].message.content;
+
+}
+
+catch(error){
+
+document.getElementById("answer").innerText =
+"حدث خطأ في الاتصال بالذكاء الاصطناعي";
+
+}
 
 }
