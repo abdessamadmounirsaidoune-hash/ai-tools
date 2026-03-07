7761532983929b56b45ea8d5c983a638b7624ac6
abdessamadmounirsaidoune-hash <abdessamadmounirsaidoune@gmail.com>
2026-03-08 00:28:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-async function askAI() {
+async function askAI(){
 
-let question = document.getElementById("question").value;
+let question =
+document.getElementById("question").value;
 
-document.getElementById("answer").innerText = "جاري التفكير...";
+let answer =
+document.getElementById("answer");
 
-try {
-
-let response = await fetch("https://cold-mountain-df10.abdessamadmounirsaidoune.workers.dev", {
-
-method: "POST",
-
-headers: {
-"Content-Type": "application/json"
-},
-
-body: JSON.stringify({
-model: "openai/gpt-3.5-turbo",
-messages: [
-{
-role: "user",
-content: question
+if(answer){
+answer.innerText="جاري التفكير...";
 }
+
+try{
+
+let response = await fetch(
+"https://cold-mountain-df10.abdessamadmounirsaidoune.workers.dev",
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+model:"openai/gpt-3.5-turbo",
+messages:[
+{role:"user",content:question}
 ]
 })
-
-});
+}
+);
 
 let data = await response.json();
 
-document.getElementById("answer").innerText =
+if(answer){
+answer.innerText =
 data.choices[0].message.content;
-
 }
 
-catch(error){
+}catch{
 
-document.getElementById("answer").innerText =
-"حدث خطأ في الاتصال بالذكاء الاصطناعي";
+if(answer){
+answer.innerText="حدث خطأ في الاتصال";
+}
 
 }
 
